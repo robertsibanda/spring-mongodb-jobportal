f8dea681e83c4fbb7410637105f1d463d1a41139
Robert Sibanda <robertsibanda20@gmail.com>
2024-11-28 15:30:14 -0500

    finished VacancyService for getAll, getById

---
+++ src/test/java/com/nx/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nx.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
